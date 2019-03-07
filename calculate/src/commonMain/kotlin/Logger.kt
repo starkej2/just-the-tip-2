d2900fc715c7ea52f60089ab1c42212e37899ba1
Jeffrey Starke <starkej2@gmail.com>
2019-03-06 22:36:35 -0500

    wip - creating logging infrastructure

---
+++ calculate/src/commonMain/kotlin/Logger.kt
@@ -0,0 +1,7 @@
+package com.binaryblacksheep.justthetip
+
+// TEMP set up logging for each platform
+// TEMP does it make sense to rename calculate module to common?
+// https://kotlinlang.org/docs/reference/multiplatform.html#how-it-works
+// https://blog.bugsnag.com/kotlin-multiplatform/
+// https://dzone.com/articles/a-practical-intro-to-kotlin-multiplatform
