61d14ae626a44278ffdc0e28bbbc18b96a528e0a
Jeffrey Starke <starkej2@gmail.com>
2019-03-11 23:02:09 -0400

    Rename calculate module to calculator

--- calculator/src/commonMain/kotlin/Logger.kt
+++ calculator/src/commonMain/kotlin/Logger.kt
@@ -1,7 +1,6 @@
 package com.binaryblacksheep.justthetip
 
 // TEMP set up logging for each platform
-// TEMP does it make sense to rename calculate module to common?
 // https://kotlinlang.org/docs/reference/multiplatform.html#how-it-works
 // https://blog.bugsnag.com/kotlin-multiplatform/
 // https://dzone.com/articles/a-practical-intro-to-kotlin-multiplatform
